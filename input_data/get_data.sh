d76f9d518e894c893d442e187876fdf69d5d7851
Bernardo Pacini <bpacini@umich.edu>
2024-10-06 21:00:47 -0700

    Fixing tests and input_data link

--- input_data/get_data.sh
+++ input_data/get_data.sh
@@ -1,2 +1,2 @@
 # Requires the "gdown" Python package 
-gdown --folder https://drive.google.com/drive/folders/1gGmAnh0QDatxPc6lU3jRNgsaRmH3svxS?usp=share_link
+gdown --folder https://drive.google.com/drive/folders/1AblaEjomcXom7n3PXT_E9oee0RcAR6au?usp=sharing
