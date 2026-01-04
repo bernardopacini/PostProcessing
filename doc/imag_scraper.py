1578c3e34ff8b1cf04644c1ea902460433ebd5a6
Bernardo Pacini <bpacini@umich.edu>
2026-01-04 11:33:30 -0800

    New approach

--- doc/imag_scraper.py
+++ doc/imag_scraper.py
@@ -2,8 +2,8 @@ from glob import glob
 import shutil
 import os
 from sphinx_gallery.scrapers import figure_rst
-from kaleido.scopes._utils import kaleido_get_chrome
-kaleido_get_chrome()
+import kaleido
+kaleido.get_chrome_sync()
 
 
 def png_scraper(block, block_vars, gallery_conf):
